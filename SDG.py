f0d2bf5cc0a4380615452ecfbb407f39781a46af
junhanyang <71431941+junhanyang@users.noreply.github.com>
2022-06-21 16:38:21 +0800

    Update SDG.py

--- SDG.py
+++ SDG.py
@@ -54,7 +54,7 @@ SDG = ['SDG 01 - No Poverty','SDG 02 - Zero Hunger','SDG 03 - Good Health and We
 'SDG 13 - Climate Action', 'SDG 14 - Life Below Water', 'SDG 15 - Life on Land', 
 'SDG 16 - Peace, Justice and Strong Insttitutions', 'SDG 17 - Partnerships for the Goals']
 images = [f'/SDG Icons/SDG0{x}.png' for x in range(1,10)]
-images.extend([f'/SDG Icons/SDG{x}.png' for x in range(10,18)])
+images.extend([f'SDG Icons/SDG{x}.png' for x in range(10,18)])
 images=dict(zip(SDG,images))
 
 Portfolios = st.multiselect('Please choose the portoflio you are interested in:',portfolios)
